ab9f1c5b1ee26f23ab2d246c6149cc2e5dee47a2
Ilona Badur <Ilona.badur@skola.botkyrka.se>
2021-03-19 15:39:23 +0100

    Hej

--- main.lua
+++ main.lua
@@ -1,14 +1,23 @@
-local current_color = {1, 1, 1, 1}
+love.load = function ()
+  x = 50
+  y = 300
+  w = 85
+  h = 100
+end
 
-love.draw = function()
-  local paddle1 = {50, 50, 50, 200, 100, 200, 100, 50}
-  local paddle2 = {750, 400, 750, 550, 700, 550, 700, 400}
+love.update = function (dt)
 
-  love.graphics.setColor(current_color)
+  if love.keyboard.isDown('right') then
+  x = x + 6
+  
 
-  love.graphics.polygon('fill', paddle1)
-  love.graphics.polygon('fill', paddle2)
+  elseif love.keyboard.isDown('left') then
+    x = x - 6
+  end
 
 end
 
+love.draw = function ()
+  love.graphics.rectangle('fill', x, y, w, h)
+end
 
